81b685994bc3cee3cab80ccb974cca175889c76b
alfianrma77 <alfianrma77@gmail.com>
2023-10-17 10:37:33 +0700

    Feat: Add Edit Dokumen

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
-import { View, Text, SafeAreaView, StatusBar } from 'react-native'
-import React from 'react'
-import Routing from './src/navigation/Routing'
+import {View, Text, SafeAreaView, StatusBar} from 'react-native';
+import React from 'react';
+import Routing from './src/navigation/Routing';
 
 const App = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
-      <StatusBar barStyle={'dark-content'}/>
-      <Routing/>
+      <StatusBar barStyle={'dark-content'} />
+      <Routing />
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default App
+export default App;
