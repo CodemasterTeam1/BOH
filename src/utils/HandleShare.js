671839d5dd371e824a13b97f59083fc37f7e0461
Ernisa <ernisarahma@gmail.com>
2023-10-17 15:07:55 +0700

    feat: add Title Header

---
+++ src/utils/HandleShare.js
@@ -0,0 +1,14 @@
+export default handleShare = () => {
+  // Logika berbagi konten
+  Share.share({
+    message: 'Ini adalah konten yang akan dibagikan.',
+  })
+    .then(result => {
+      if (result.action === Share.sharedAction) {
+        console.log('Konten dibagikan.');
+      } else if (result.action === Share.dismissedAction) {
+        console.log('Berbagi dibatalkan.');
+      }
+    })
+    .catch(error => console.log('Error', error));
+};
