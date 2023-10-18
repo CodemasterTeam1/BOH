57cc1df3e6ec4c4b2c28a4cdb29049b0d470a68d
4r71x123 <farhanharvito@gmail.com>
2023-10-18 13:46:13 +0700

    feat: added card component for home and insentif

--- src/screen/home/HomeScreen.js
+++ src/screen/home/HomeScreen.js
@@ -1,10 +1,18 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import HomeCard from '../../component/sections/HomeCard';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text>Home</Text>
+      <HomeCard
+        kode="12321312"
+        tanggal="Sen, 17 Sep 2018 - 10:30"
+        mobil="Avanza"
+        nama="Handoko"
+        role="salesman"
+        type="subsidi"
+      />
     </View>
   );
 };
--- src/screen/insentif/InsentifScreen.js
+++ src/screen/insentif/InsentifScreen.js
@@ -1,11 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import InsentifCard from '../../component/sections/InsentifCard';
 
 const InsentifScreen = () => {
   return (
-    <View>
-      <Text>Insentif</Text>
-    </View>
+    <InsentifCard
+      tanggal="Sen, 17 Sep 2018 - 10:30"
+      insentif="Rp 1.000.000"
+      booking="2"
+      type="validate"
+    />
   );
 };
 
