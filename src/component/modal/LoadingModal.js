c7c9190e5690c7176383c3d52a452c585f980360
alfianrma77 <alfianrma77@gmail.com>
2023-10-18 10:31:13 +0700

    feat: add LoadingModal

--- src/component/modal/LoadingModal.js
+++ src/component/modal/LoadingModal.js
@@ -1,8 +1,31 @@
-import {View, Text, Modal} from 'react-native';
+import {View, Text, Modal, ActivityIndicator, StyleSheet} from 'react-native';
 import React from 'react';
+import {Primary} from '../../styles/Colour';
 
-const LoadingModal = isloading => {
-  return <Modal visible={isloading}></Modal>;
+const LoadingModal = ({isloading}) => {
+  return (
+    <Modal visible={isloading}>
+      <View style={styles.screen}>
+        <View style={styles.body}>
+          <ActivityIndicator size="large" color={Primary} />
+        </View>
+      </View>
+    </Modal>
+  );
 };
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  body: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+});
 
 export default LoadingModal;
