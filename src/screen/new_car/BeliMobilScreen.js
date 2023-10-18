7ada5d17ca7df9e6bdb5cd5e2d1a33f2930fe35f
Farhan <mufr31236@gmail.com>
2023-10-18 10:07:50 +0700

    chore : add placeholder for screen and new navigation

---
+++ src/screen/new_car/BeliMobilScreen.js
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const BeliMobilScreen = () => {
+  return (
+    <View>
+      <Text>BeliMobilScreen</Text>
+    </View>
+  );
+};
+
+export default BeliMobilScreen;
