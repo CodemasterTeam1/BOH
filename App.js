308c3aa27cff5626299bf8791a740d29235dad62
alfianrma77 <alfianrma77@gmail.com>
2023-10-19 20:21:22 +0700

    feat : add Riwayat and Daftar Booking

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import Routing from './src/navigation/Routing';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import {persistor, store} from './src/data/local/Store';
+import DetailInsentifScreen from './src/screen/insentif/DetailInsentifScreen';
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <PersistGate persistor={persistor}>
         <SafeAreaView style={{flex: 1}}>
           <StatusBar barStyle={'dark-content'} />
-          <Routing />
+          <DetailInsentifScreen />
         </SafeAreaView>
       </PersistGate>
     </Provider>
